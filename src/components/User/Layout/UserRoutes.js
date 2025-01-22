11ff370285656b7c4e3bd72d12570151e489201e
vignesh <vignesh@kmdigicommerce.com>
2025-01-22 13:21:07 +0530

    Updated sidebar

--- src/components/User/Layout/UserRoutes.js
+++ src/components/User/Layout/UserRoutes.js
@@ -4,6 +4,9 @@ import UserDashboard from "../Dashboard/UserDashboard";
 import AttendanceRequest from '../Attendance/AttendanceRequest';
 import AttendanceHistory from '../Attendance/AttendanceHistory';
 import LeaveRequest from '../Leave/LeaveRequest';
+import Payslip from '../Payroll/Payslip';
+import Allowance from '../Payroll/Allowance';
+import Deduction from '../Payroll/Deductions';
 import { Box } from '@mui/material';
 
 function UserRoutes() {
@@ -16,6 +19,9 @@ function UserRoutes() {
           <Route path="/attendance/request" element={<AttendanceRequest />} />
           <Route path="/attendance/history" element={<AttendanceHistory />} />
           <Route path="/leave/request" element={<LeaveRequest />} /> 
+          <Route path="/payroll/payslips" element={<Payslip />} /> 
+          <Route path="/payroll/view-allowances" element={<Allowance />} /> 
+          <Route path="/payroll/view-deductions" element={<Deduction />} /> 
         </Routes>
     </Box>
   )
